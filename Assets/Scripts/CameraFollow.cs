1b4554a067178f0aa70de6b76314a2c40423dc35
Muhammed <mmeral3382@gmail.com>
2023-09-01 13:03:03 +0300

    8.Hafta Ödevi

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] Transform targetObject;
+
+    [SerializeField] Vector3 cameraOffet;
+
+
+    private void Update()
+    {
+        
+    }
+
+}
